ea32156e091bc53be7455005e8b5efc94974b251
EdoardoMenicucci <edoardo.menicucci@libero.it>
2025-02-06 11:38:58 +0100

    Upcoming appointment implementation, minor ui changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,7 +20,7 @@ import { AuthService } from '../auth/auth.service';
     DxCircularGaugeModule,
     SidebarComponent,
     DxListModule,
-    CommonModule
+    CommonModule,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
@@ -32,12 +32,20 @@ export class DashboardComponent implements OnInit {
   cancellationRate = 0;
   upcomingAppointments: any[] = [];
   currentDate = new Date();
-  username : string | null = null;
+  username: string | null = null;
 
+  palette: string[] = [
+    // '#818CF8', // accent-light (primary)
+    '#4F46E5', // accent
+    '#4338CA', // accent-hover
+  ];
 
+  gaugePalette: string[] = ['#059669', '#fde300', '#DC2626'];
 
-
-  constructor(private dashboardService: DashboardService, private authService : AuthService) {
+  constructor(
+    private dashboardService: DashboardService,
+    private authService: AuthService
+  ) {
     // prepara i dati per il grafico
     this.dashboardService.statistics$.subscribe((data) => {
       this.statisticsData = [
@@ -67,6 +75,7 @@ export class DashboardComponent implements OnInit {
 
       this.cancellationRate = data.cancellationRate;
       this.username = this.authService.username;
+      this.upcomingAppointments = data.upcomingAppointmentsList;
       console.log('Statistics data recived:', data);
     });
   }
@@ -76,9 +85,7 @@ export class DashboardComponent implements OnInit {
   };
 
   formatUserName(username: string | null): string {
-    return firstLetterToUpperCase(username
-      ? username
-      : 'User');
+    return firstLetterToUpperCase(username ? username : 'User');
   }
 
   formatDate(dateInput: string | Date | null): string {
@@ -87,16 +94,16 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     // Esempio di dati per upcoming appointments
-    this.upcomingAppointments = [
-      {
-        id: 1,
-        text: 'Meeting with Client',
-        date: '2023-10-15',
-        time: '10:00',
-      },
-      { id: 2, text: 'Team Standup', date: '2023-10-16', time: '09:30' },
-      { id: 3, text: 'Project Review', date: '2023-10-17', time: '11:00' },
-      { id: 4, text: 'Design Session', date: '2023-10-18', time: '14:00' },
-    ];
+    // this.upcomingAppointments = [
+    //   {
+    //     id: 1,
+    //     text: 'Meeting with Client',
+    //     date: '2023-10-15',
+    //     time: '10:00',
+    //   },
+    //   { id: 2, text: 'Team Standup', date: '2023-10-16', time: '09:30' },
+    //   { id: 3, text: 'Project Review', date: '2023-10-17', time: '11:00' },
+    //   { id: 4, text: 'Design Session', date: '2023-10-18', time: '14:00' },
+    // ];
   }
 }
